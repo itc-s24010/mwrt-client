75ca16be8ccd0d17c8898d6cc94057d15114e45a
s24004 <s24004@std.it-college.ac.jp>
2026-01-26 14:03:52 +0900

    タイマー機能のテスト

--- app/test/content.tsx
+++ app/test/content.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Timer } from "@/page_components/app/timer";
 import { PlainButton } from "@/page_components/form/button";
 import { PopupForm } from "@/page_components/form/popup_form";
 import { PlainTitle } from "@/page_components/title";
@@ -8,15 +9,13 @@ import { useState } from "react";
 export function PageContent() {
     const [showPopup, setShowPopup] = useState(false);
 
+    const [lapTimes, setLapTimes] = useState<number[]>([]);
+
 
     return (
         <div>
             <PlainTitle> Test Page </PlainTitle>
-            <PlainButton customAttributes={
-                {
-                    onClick: () => { setShowPopup(!showPopup);}
-                }
-            }>
+            <PlainButton onClick={() => { setShowPopup(!showPopup)}}>
                 ポップアップテスト
             </PlainButton>
             {
@@ -33,6 +32,29 @@ export function PageContent() {
                     <input id="testInput" type="text" />
                 </PopupForm>
             }
+
+
+
+            <Timer custom={{
+                onStart: () => {
+                    console.log("Timer started");
+                },
+                onStop: (time) => {
+                    console.log("Timer stopped at", time);
+                },
+                allowLap: true,
+                onLap: (time) => {
+                    setLapTimes((prev) => [...prev, time]);
+                }
+            }}></Timer>
+
+            <div>
+                {
+                    lapTimes.map((time, index) => (
+                        <div key={index}>Lap {index + 1}: {(time / 1000).toFixed(2)}s</div>
+                    ))
+                }
+            </div>
             
         </div>
     )
